fc6d3f706af572cc8a40c8bdc043279088ab27f6
jgxjgx1 <155945078+jgxjgx1@users.noreply.github.com>
2024-01-08 17:56:50 +0200

    Viikon 1 tehtävät

---
+++ vk1_t1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int game = 1;
+    int satunnaisluku;
+    int arvaus;
+
+    srand(42); // Asetetaan siemenluku
+    satunnaisluku = rand() % 20 + 1; // Satunnaisluku väliltä 0-19 +1 => väli 1-20
+
+    while(game == 1){
+        cout << "Arvaa luku: ";
+        cin >> arvaus;
+
+        if(arvaus < satunnaisluku) cout << "Luku on suurempi" << endl;
+        if(arvaus > satunnaisluku) cout << "Luku on pienempi" << endl;
+
+        if(arvaus == satunnaisluku){
+            game = 0;
+            cout << "Oikein!" << endl;
+        }
+    }
+
+    return 0;
+}
