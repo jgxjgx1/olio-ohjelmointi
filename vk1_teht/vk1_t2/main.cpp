299377b110ae772c8eaef562df281d7a21bb51de
jgxjgx1 <155945078+jgxjgx1@users.noreply.github.com>
2024-01-15 08:32:07 +0200

    vk1 teht

---
+++ vk1_teht/vk1_t2/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void peli();
+
+
+int main()
+{
+    peli();
+    return 0;
+}
+
+void peli()
+{
+    int game = 1;
+    int satunnaisluku;
+    int arvaus;
+
+    srand(time(0)); // Asetetaan siemenluku
+    satunnaisluku = rand() % 20 + 1; // Satunnaisluku väliltä 0-19 +1 => väli 1-20
+
+    while(game == 1){
+        cout << "Arvaa luku: ";
+        cin >> arvaus;
+
+        if(arvaus < satunnaisluku) cout << "Luku on suurempi" << endl;
+        if(arvaus > satunnaisluku) cout << "Luku on pienempi" << endl;
+
+        if(arvaus == satunnaisluku){
+            game = 0;
+            cout << "Oikein!" << endl;
+        }
+    }
+}
